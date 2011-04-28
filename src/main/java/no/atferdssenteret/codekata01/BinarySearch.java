f4ae8eae8786955e41e3b6392f3b831a8c11abbf
Bjorn Arild Kristiansen <bjornakr@gmail.com>
2011-04-28 16:20:14 +0200

    Commented the BinarySearch interface

--- src/main/java/no/atferdssenteret/codekata01/BinarySearch.java
+++ src/main/java/no/atferdssenteret/codekata01/BinarySearch.java
@@ -3,5 +3,13 @@ package no.atferdssenteret.codekata01;
 public interface BinarySearch {
     public final static int VALUE_NOT_FOUND = -1;
 
+    /**
+     * 
+     * Searches for a value in an array of integers.
+     * 
+     * @param valueToSearchFor The value to search for
+     * @param allValues The array to search in 
+     * @return The position of the value in the array
+     */
     public int chop(int valueToSearchFor, int[] allValues);
 }
